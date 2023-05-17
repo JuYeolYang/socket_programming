890948c8e92a6609ba2858a0f4a975751ea129bf
jeongwushin <101108626+jeongwushin@users.noreply.github.com>
2023-05-17 14:45:12 +0900

    final

--- HW3_Client_Final.py
+++ HW3_Client_Final.py
@@ -1,37 +1,30 @@
-#전체 검색은 되지만 삽입 수정 등은 막힘 해결 필요
 import socket
-import time
 
-# 서버 설정
-HOST = 'localhost'
-PORT = 8000
+# 서버 정보
+SERVER_HOST = '172.31.39.218'
+SERVER_PORT = 8000
 
-# 소켓 생성
+# 서버에 연결
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+client_socket.connect((SERVER_HOST, SERVER_PORT))
+print('DNS 서버에 연결되었습니다.')
 
 while True:
     # 사용자 입력 받기
-    request = input('요청을 입력하세요(function domain ip)\n')
+    request = input('요청을 입력하세요 ("DISCONNECT"로 연결 종료): ')
 
-    # 요청 전송
+    # 서버로 요청 전송
     client_socket.sendall(request.encode())
 
-    # 응답 수신
-    time.sleep(0.1)
+    # 연결 종료 요청인 경우 루프 종료
+    if request == 'DISCONNECT':
+        break
+
+    # 서버로부터 응답 수신
     response = client_socket.recv(1024).decode()
 
     # 응답 출력
     print('서버 응답:', response)
 
-    # 도메인 검색인 경우, IP 주소 출력
-    if response.startswith('해당 도메인이 존재합니다.'):
-        _, ip_address = response.split(':')
-        print('IP 주소:', ip_address)
-
-    # 종료 조건
-    if request == 'EXIT':
-        break
-
-# 소켓 연결 종료
+# 클라이언트 소켓 연결 종료
 client_socket.close()
--- HW3_Server_Final.py
+++ HW3_Server_Final.py
@@ -1,5 +1,6 @@
 import socket
 import pymysql
+import threading
 
 # MySQL 연결 설정
 db = pymysql.connect(
@@ -12,10 +13,10 @@ db = pymysql.connect(
 
 # DNS 테이블 생성
 cursor = db.cursor()
-cursor.execute("CREATE TABLE IF NOT EXISTS domains (domain VARCHAR(255), ip VARCHAR(255))")
+cursor.execute("CREATE TABLE IF NOT EXISTS DNS (domain VARCHAR(255), ip VARCHAR(255))")
 
 # 서버 설정
-HOST = 'localhost'
+HOST = '0.0.0.0'
 PORT = 8000
 
 # 소켓 생성
@@ -25,72 +26,82 @@ server_socket.listen()
 
 print('DNS 서버가 시작되었습니다.')
 
+def handle_client(client_socket):
+    while True:
+        # 클라이언트 요청 수신
+        request = client_socket.recv(1024).decode()
+
+        # 요청 처리
+        if request.startswith('INSERT'):
+            # INSERT 요청 처리
+            _, domain, ip = request.split()
+            sql = f"INSERT INTO DNS (domain, ip) VALUES (%s, %s)"
+            values = (domain, ip)
+            cursor.execute(sql, values)
+            db.commit()
+            response = 'DNS 데이터가 추가되었습니다.'
+        elif request.startswith('UPDATE'):
+            # UPDATE 요청 처리
+            _, domain, ip = request.split()
+            sql = f"UPDATE DNS SET ip = %s WHERE domain = %s"
+            values = (ip, domain)
+            cursor.execute(sql, values)
+            db.commit()
+            if cursor.rowcount > 0:
+                response = 'DNS 데이터가 수정되었습니다.'
+            else:
+                response = '해당 도메인이 존재하지 않습니다.'
+        elif request.startswith('DELETE'):
+            # DELETE 요청 처리
+            _, domain = request.split()
+            sql = "DELETE FROM DNS WHERE domain = %s"
+            values = (domain,)
+            cursor.execute(sql, values)
+            db.commit()
+            if cursor.rowcount > 0:
+                response = 'DNS 데이터가 삭제되었습니다.'
+            else:
+                response = '해당 도메인이 존재하지 않습니다.'
+        elif request.startswith('QUERY'):
+            # QUERY 요청 처리
+            _, domain = request.split()
+            sql = "SELECT ip FROM DNS WHERE domain = %s"
+            values = (domain,)
+            cursor.execute(sql, values)
+            result = cursor.fetchone()
+            if result is not None:
+                response = f'{domain}의 IP 주소: {result[0]}'
+            else:
+                response = '해당 도메인이 존재하지 않습니다.'
+        elif request == 'SEARCH ALL':
+            # SEARCH ALL 요청 처리
+            sql = "SELECT domain, ip FROM DNS"
+            cursor.execute(sql)
+            results = cursor.fetchall()
+            if len(results) > 0:
+                response = 'DNS 데이터 검색 결과:\n'
+                for row in results:
+                    response += f'{row[0]}: {row[1]}\n'
+            else:
+                response = 'DNS 데이터가 존재하지 않습니다.'
+        else:
+            response = '잘못된 요청입니다.'
+
+        # 클라이언트에 응답 전송
+        client_socket.sendall(response.encode())
+
+        # 클라이언트와의 연결 종료
+        if request == 'DISCONNECT':
+            break
+
+    client_socket.close()
+
 while True:
     # 클라이언트로부터 연결 요청 대기
     client_socket, addr = server_socket.accept()
     print('클라이언트가 연결되었습니다:', addr)
 
-    # 클라이언트 요청 수신
-    request = client_socket.recv(1024).decode()
+    # 클라이언트 요청을 처리하는 스레드 생성
+    client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+    client_thread.start()
 
-    # 요청 처리
-    if request.startswith('INSERT'):
-        # INSERT 요청 처리
-        _, domain, ip = request.split()
-        sql = f"INSERT INTO domains (domain, ip) VALUES (%s, %s)"
-        values = (domain, ip)
-        cursor.execute(sql, values)
-        db.commit()
-        response = 'DNS 데이터가 추가되었습니다.'
-    elif request.startswith('UPDATE'):
-        # UPDATE 요청 처리
-        _, domain, ip = request.split()
-        sql = f"UPDATE domains SET ip = %s WHERE domain = %s"
-        values = (ip, domain)
-        cursor.execute(sql, values)
-        db.commit()
-        if cursor.rowcount > 0:
-            response = 'DNS 데이터가 수정되었습니다.'
-        else:
-            response = '해당 도메인이 존재하지 않습니다.'
-    elif request.startswith('DELETE'):
-        # DELETE 요청 처리
-        _, domain = request.split()
-        sql = "DELETE FROM domains WHERE domain = %s"
-        values = (domain,)
-        cursor.execute(sql, values)
-        db.commit()
-        if cursor.rowcount > 0:
-            response = 'DNS 데이터가 삭제되었습니다.'
-        else:
-            response = '해당 도메인이 존재하지 않습니다.'
-    elif request.startswith('QUERY'):
-        # QUERY 요청 처리
-        _, domain = request.split()
-        sql = "SELECT ip FROM domains WHERE domain = %s"
-        values = (domain,)
-        cursor.execute(sql, values)
-        result = cursor.fetchone()
-        if result is not None:
-            response = f'{domain}의 IP 주소: {result[0]}'
-        else:
-            response = '해당 도메인이 존재하지 않습니다.'
-    elif request == 'SEARCH ALL':
-        # SEARCH ALL 요청 처리
-        sql = "SELECT domain, ip FROM domains"
-        cursor.execute(sql)
-        results = cursor.fetchall()
-        if len(results) > 0:
-            response = 'DNS 데이터 검색 결과:\n'
-            for row in results:
-                response += f'{row[0]}: {row[1]}\n'
-        else:
-            response = 'DNS 데이터가 존재하지 않습니다.'
-    else:
-        response = '잘못된 요청입니다.'
-
-    # 클라이언트에 응답 전송
-    client_socket.sendall(response.encode())
-
-    # 클라이언트 소켓 연결 종료
-    client_socket.close()
